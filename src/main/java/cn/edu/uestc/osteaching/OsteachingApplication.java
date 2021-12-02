6ed2eb4a1c4dca500ac177ecdc64dcb6599b59c4
Erewhon-Lucy <1663523296@qq.com>
2021-12-02 13:49:49 +0800

    first commit

---
+++ src/main/java/cn/edu/uestc/osteaching/OsteachingApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.uestc.osteaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsteachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsteachingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/edu/uestc/osteaching/entity/RetResponse.java
@@ -0,0 +1,25 @@
+package cn.edu.uestc.osteaching.entity;
+
+public class RetResponse {
+    private final static String SUCCESS = "success";
+
+    public static <T> RetResult<T> makeOKRsp() {
+        return new RetResult<T>().setCode(RetCode.SUCCESS).setMsg(SUCCESS);
+    }
+
+    public static <T> RetResult<T> makeOKRsp(T data) {
+        return new RetResult<T>().setCode(RetCode.SUCCESS).setMsg(SUCCESS).setData(data);
+    }
+
+    public static <T> RetResult<T> makeErrRsp(String message) {
+        return new RetResult<T>().setCode(RetCode.FAIL).setMsg(SUCCESS);
+    }
+
+    public static <T> RetResult<T> makeRsp(int code, String msg) {
+        return new RetResult<T>().setCode(code).setMsg(msg);
+    }
+
+    public static <T> RetResult<T> makeRsp(int code, String msg, T data) {
+        return new RetResult<T>().setCode(code).setMsg(msg).setData(data);
+    }
+}
