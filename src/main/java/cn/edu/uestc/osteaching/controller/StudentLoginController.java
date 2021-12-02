ff586d5c5008d0d35438d1b964e272c23c9f6ba6
Erewhon-Lucy <1663523296@qq.com>
2021-12-02 15:54:52 +0800

    update:StudentLoginController

--- src/main/java/cn/edu/uestc/osteaching/controller/StudentLoginController.java
+++ src/main/java/cn/edu/uestc/osteaching/controller/StudentLoginController.java
@@ -3,7 +3,7 @@ package cn.edu.uestc.osteaching.controller;
 import cn.edu.uestc.osteaching.entity.RetResponse;
 import cn.edu.uestc.osteaching.entity.RetResult;
 import cn.edu.uestc.osteaching.entity.T_Student;
-import cn.edu.uestc.osteaching.repository.T_StudentRepository;
+import cn.edu.uestc.osteaching.repository.StudentLoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +16,10 @@ import java.util.List;
 @RequestMapping("/student")
 public class StudentLoginController {
     @Autowired
-    private T_StudentRepository studentRepository;
+    private StudentLoginRepository studentLoginRepository;
 
     @GetMapping("/login/{name}/{password}")
     public RetResult<List<T_Student>> findByNameAndPassword(@PathVariable("name") String name, @PathVariable("password") String password) {
-        return RetResponse.makeOKRsp(studentRepository.findByNameAndPassword(name,password));
+        return RetResponse.makeOKRsp(studentLoginRepository.findByNameAndPassword(name,password));
     }
 }
