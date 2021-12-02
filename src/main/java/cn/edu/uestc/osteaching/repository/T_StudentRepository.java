710b5ce9ec45a83d893ae5cb5be0c0e3c1ce9046
Erewhon-Lucy <1663523296@qq.com>
2021-12-02 15:54:17 +0800

    update:T_StudentRepository

--- src/main/java/cn/edu/uestc/osteaching/repository/T_StudentRepository.java
+++ src/main/java/cn/edu/uestc/osteaching/repository/T_StudentRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface T_StudentRepository extends JpaRepository<T_Student, Integer> {
-
-    List<T_Student> findByNameAndPassword(String name, String password);
 }
