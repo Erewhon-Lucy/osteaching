f05ba347f9b6b9dc3c1e6a5e6de880b1cd8942a7
littleppi <2635105716@qq.com>
2021-12-05 16:55:59 +0800

    Course增删改查功能

---
+++ src/main/java/cn/edu/uestc/osteaching/entity/T_Course.java
@@ -0,0 +1,28 @@
+package cn.edu.uestc.osteaching.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+public class T_Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cid;
+    private String name;
+    private Integer hour;
+    private String photo;
+    private String video;
+    private String introduction;
+    private String homework;
+    private String appendix;
+    private Date subtime;
+    private Date date;
+    private String place;
+    private Integer tid;
+}
