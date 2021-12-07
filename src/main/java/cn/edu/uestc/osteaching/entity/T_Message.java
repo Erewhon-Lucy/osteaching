aa84445127c8b91ccdfe11346009aa1d7e3bb303
Erewhon-Lucy <1663523296@qq.com>
2021-12-07 11:40:56 +0800

    edit:T_Message.java

--- src/main/java/cn/edu/uestc/osteaching/entity/T_Message.java
+++ src/main/java/cn/edu/uestc/osteaching/entity/T_Message.java
@@ -1,8 +1,18 @@
 package cn.edu.uestc.osteaching.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
+@Data
 public class T_Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer mid;
     private String title;
     private String content;
