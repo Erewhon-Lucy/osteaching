7cd9ddc2abd695748bdeaddeab35b7cc47fe5c08
Erewhon-Lucy <1663523296@qq.com>
2021-12-07 14:34:15 +0800

    edit:StudentRegisterController.java 修改StudentRegisterController.java,添加了默认Read_count

--- src/main/java/cn/edu/uestc/osteaching/controller/StudentRegisterController.java
+++ src/main/java/cn/edu/uestc/osteaching/controller/StudentRegisterController.java
@@ -13,12 +13,12 @@ public class StudentRegisterController {
     @Autowired
     private T_StudentRepository studentRegister;
 
-
     @GetMapping("/{name}/{password}")
     public RetResult<T_Student> save(@PathVariable("name") String name, @PathVariable("password") String password){
         T_Student student = new T_Student();
         student.setName(name);
         student.setPassword(password);
+        student.setRead_count(0);
         return RetResponse.makeOKRsp(studentRegister.save(student));
     }
 }
