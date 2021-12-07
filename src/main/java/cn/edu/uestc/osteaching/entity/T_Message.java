11664e429ffe7aa950baa52240a079141ef4d6ea
Erewhon-Lucy <1663523296@qq.com>
2021-12-07 11:40:14 +0800

    init:T_Message.java

---
+++ src/main/java/cn/edu/uestc/osteaching/entity/T_Message.java
@@ -0,0 +1,14 @@
+package cn.edu.uestc.osteaching.entity;
+
+import java.util.Date;
+
+public class T_Message {
+    private Integer mid;
+    private String title;
+    private String content;
+    private Date date;
+    private Integer count_good;
+    private Integer sid;
+    private Integer tid;
+    private Integer parentid;
+}
