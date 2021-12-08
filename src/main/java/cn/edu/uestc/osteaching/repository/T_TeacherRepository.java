207c9c667087ee0ff3aecb7b5116d34d4f2c60aa
Erewhon-Lucy <1663523296@qq.com>
2021-12-08 22:31:02 +0800

    init:T_TeacherRepository.java

---
+++ src/main/java/cn/edu/uestc/osteaching/repository/T_TeacherRepository.java
@@ -0,0 +1,7 @@
+package cn.edu.uestc.osteaching.repository;
+
+import cn.edu.uestc.osteaching.entity.T_Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface T_TeacherRepository extends JpaRepository<T_Teacher, Integer> {
+}
