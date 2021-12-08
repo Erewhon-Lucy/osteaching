591b83e4ade8cfd4cc9c548fb3957598a73ed78d
Erewhon-Lucy <1663523296@qq.com>
2021-12-08 22:32:23 +0800

    edit:T_TeacherRepository.java 定义findByNameAndPassword方法

--- src/main/java/cn/edu/uestc/osteaching/repository/T_TeacherRepository.java
+++ src/main/java/cn/edu/uestc/osteaching/repository/T_TeacherRepository.java
@@ -1,7 +1,11 @@
 package cn.edu.uestc.osteaching.repository;
 
+import cn.edu.uestc.osteaching.entity.T_Student;
 import cn.edu.uestc.osteaching.entity.T_Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface T_TeacherRepository extends JpaRepository<T_Teacher, Integer> {
+    List<T_Teacher> findByNameAndPassword(String name, String password);
 }
