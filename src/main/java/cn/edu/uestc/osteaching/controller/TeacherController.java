248761f80dd9d49e735845f56044c7eafe9268c2
Erewhon-Lucy <1663523296@qq.com>
2021-12-08 22:56:59 +0800

    edit:TeacherController.java 添加教师注册接口

--- src/main/java/cn/edu/uestc/osteaching/controller/TeacherController.java
+++ src/main/java/cn/edu/uestc/osteaching/controller/TeacherController.java
@@ -2,6 +2,7 @@ package cn.edu.uestc.osteaching.controller;
 
 import cn.edu.uestc.osteaching.entity.RetResponse;
 import cn.edu.uestc.osteaching.entity.RetResult;
+import cn.edu.uestc.osteaching.entity.T_Student;
 import cn.edu.uestc.osteaching.entity.T_Teacher;
 import cn.edu.uestc.osteaching.repository.T_TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,13 @@ public class TeacherController {
     @GetMapping("/login/{name}/{password}")
     public RetResult<List<T_Teacher>> login(@PathVariable("name") String name, @PathVariable("password") String password) {
         return RetResponse.makeOKRsp(teacherRepository.findByNameAndPassword(name,password));
+    }
+
+    @GetMapping("/register/{name}/{password}")
+    public RetResult<T_Teacher> save(@PathVariable("name") String name, @PathVariable("password") String password){
+        T_Teacher teacher = new T_Teacher();
+        teacher.setName(name);
+        teacher.setPassword(password);
+        return RetResponse.makeOKRsp(teacherRepository.save(teacher));
     }
 }
