e94368ae9bf103d5b0a9afcd5acc0b650e057010
Erewhon-Lucy <1663523296@qq.com>
2021-12-14 14:48:34 +0800

    edit:T_StudentRepository.java 完成全体学生read_count变为1方法测试

--- src/main/java/cn/edu/uestc/osteaching/repository/T_StudentRepository.java
+++ src/main/java/cn/edu/uestc/osteaching/repository/T_StudentRepository.java
@@ -2,8 +2,14 @@ package cn.edu.uestc.osteaching.repository;
 
 import cn.edu.uestc.osteaching.entity.T_Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import javax.transaction.Transactional;
 
 public interface T_StudentRepository extends JpaRepository<T_Student, Integer> {
+    @Modifying
+    @Transactional
+    @Query("update T_Student set read_count = 1")
+    void updateAllReadCountOne();
 }
